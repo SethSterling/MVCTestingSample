199996f57c4a31a3c071328f6fdcb4da4ff625ec
SethSterling <63669378+SethSterling@users.noreply.github.com>
2020-11-14 19:45:31 -0800

    Set up website data store (#4)
    * Created product model
    
    * Created DbContext
    
    * Created ProductRepository and default implementation
    
    * Add reference to EntityFramework Core
    
    * Set up database connection
    
    * Implemented ProductRepository

---
+++ MVCTestingSample/Models/EFProductRepository.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCTestingSample.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCTestingSample.Models
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private readonly ProductDbContext _context;
+
+        public EFProductRepository(ProductDbContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Adds a Product to the data store
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public Task AddProductAsync(Product p)
+        {
+            _context.Add(p);
+            return _context.SaveChangesAsync();
+        }
+
+        public Task DeleteProductAsync(Product p)
+        {
+            _context.Remove(p);
+            return _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Product>> GetAllProductsAsync()
+        {
+            return await _context.Products
+                           .OrderBy(p => p.Name)
+                           .ToListAsync();
+        }
+
+        /// <summary>
+        /// Returns a product by the ID, or null if
+        /// no product matches
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<Product> GetProductByIdAsync(int id)
+        {
+            return await _context.Products
+                           .Where(p => p.ProductId == id)
+                           .SingleOrDefaultAsync();
+        }
+
+        public Task UpdateProductAsync(Product p)
+        {
+            _context.Entry(p).State = EntityState.Modified;
+            return _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ MVCTestingSample/Models/Interfaces/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCTestingSample.Models.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetProductByIdAsync(int id);
+
+        Task<List<Product>> GetAllProductsAsync();
+
+        Task AddProductAsync(Product p);
+        Task UpdateProductAsync(Product p);
+        Task DeleteProductAsync(Product p);
+    }
+}
