0e086cadabdc9cf2a7d5a990b1b41aa92e52c5ae
SethSterling <sethbdsterling@gmail.com>
2020-11-15 18:35:46 -0800

    Added unit test for ProductsController.Index

---
+++ MVCTestingSampleTests/Controllers/ProductsControllerTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MVCTestingSample.Controllers;
+using MVCTestingSample.Models;
+using MVCTestingSample.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCTestingSample.Controllers.Tests
+{
+    [TestClass()]
+    public class ProductsControllerTests
+    {
+        [TestMethod()]
+        public async Task Index_ReturnsAViewResult_WithAListOfAllProducts()
+        {
+            // Arrange
+            Mock<IProductRepository> mockRepo = new Mock<IProductRepository>();
+            mockRepo.Setup(repo => repo.GetAllProductsAsync())
+                    .ReturnsAsync(GetProducts());
+
+            ProductsController prodController = new ProductsController(mockRepo.Object);
+
+            // Act
+            IActionResult result = await prodController.Index();
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+
+        private List<Product> GetProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    ProductId = 1, Name = "Computer", Price = "199.99"
+                },
+                new Product()
+                {
+                    ProductId = 2, Name = "Shoe", Price = "599.99"
+                },
+                new Product()
+                {
+                    ProductId = 3, Name = "Desk", Price = "200"
+                }
+            };
+        }
+    }
+}
